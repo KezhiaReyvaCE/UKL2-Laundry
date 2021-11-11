65ec0a5c7d4c6998b8e4665477631b293c49a4ef
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-11-11 09:24:49 +0700

    Create Client.java

---
+++ UKL2Laundry/Client.java
@@ -0,0 +1,71 @@
+
+import java.util.ArrayList;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Client implements User{
+ private ArrayList<String> namaClient = new ArrayList<String>();
+ private ArrayList<String> alamat = new ArrayList<String>();
+ private ArrayList<String> telepon = new ArrayList<String>();
+ private ArrayList<Integer> saldo = new ArrayList<Integer>();
+ 
+ public Client() {
+ this.namaClient.add("Kezhia");
+ this.alamat.add("Ngawi");
+ this.telepon.add("0843621176");
+ this.saldo.add(250000);
+ 
+ this.namaClient.add("Zacky");
+ this.alamat.add("Madiun");
+ this.telepon.add("0236738219");
+ this.saldo.add(500000);
+ }
+ public void setSaldo(int saldo){
+ this.saldo.add(saldo);
+ }
+ public int getSaldo(int idClient){
+ return this.saldo.get(idClient);
+ } 
+ public void editSaldo(int idClient, int saldo){
+ this.saldo.set(idClient, saldo);;
+ } 
+ public int getJmlClient(){
+ return this.saldo.size();
+ } 
+ @Override
+ public void setNama(String namaClient ){
+ this.namaClient.add(namaClient);
+ }
+ 
+ @Override
+ public void setAlamat(String alamat ){
+ this.alamat.add(alamat);
+ }
+ @Override
+ public void setTelepon(String telepon ){
+ this.telepon.add(telepon);
+ }
+ 
+ @Override
+ public String getNama(int idClient){
+ return this.namaClient.get(idClient);
+ }
+ 
+ @Override
+ public String getAlamat(int idClient){
+ return this.alamat.get(idClient);
+ }
+ 
+ @Override
+ public String getTelepon(int idClient){
+ return this.telepon.get(idClient);
+ } 
+}
